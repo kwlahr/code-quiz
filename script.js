f2b208f1205cc60329ad23840e34415c13459b11
kwlahr <kwlahr@gmail.com>
2019-10-06 19:12:14 -0600

    added content to index.html and styles.css, began to psuedo-code script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+//create variable for questions and correct answers
+//create array of questions to append to container sequentially
+//create variable for score and if input=(".correct"){score++} else{timer-("15000ms")}
