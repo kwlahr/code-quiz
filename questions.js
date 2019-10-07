53e23136476c3d240ee1fc9c4c46b583c05a8f55
kwlahr <kwlahr@gmail.com>
2019-10-06 22:34:20 -0600

    added questions.js

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+      title: "What are the building blocks of the web",
+      choices: ["HTML, CSS, and Javascript", "Ed, Edd, and Eddy", "Provolone, Gouda, and Feta", "Spiders, Silk Worms, and Fishermen"],
+      answer: "HTML, CSS, and Javascript"
+    },
+    {
+      title: "What does HTML stand for?",
+      choices: ["Hypertext Markup Language", "Hypertext Madeup Language", "Hypertext Markup Lorem", "Hot Tamales Make Life"],
+      answer: "parentheses"
+    },
+    {
+        title: "What is CSS?",
+        choices: ["A coding language used to add styles to HTML", "A Columbian Warship", "That dude that wrote the Chronicles of Narnia?", "A secret government code used during World War 2"],
+        answer: "A coding language used to add styles to HTML"
+    }
+    {
+        title: "What is a Web Browser?",
+        choices: ["A software application that allows a user to access websites via a URL", "A person who is obsessed with spiders", "A coding language", "A GUI for designing videogames"],
+        answer: "A software application that allows a user to access websites via a URL"
+    }
+    {
+        title: "True or False: Javascript makes me want to drink.",
+        choices: ["True", "False"],
+        answer: "True"
+    }
+    ///etc.
+  ];
+  
--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+//create variable for score and timer and if input=(".correct"){score++} else{timer-("15000ms")}
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+//can't get this to work
+document.getElementById("start").addEventListener('click',startTimer (duration, display) {
+    var oneFifteen = 60 * 1.25,
+        display = document.querySelector('#timer');
+    startTimer(oneFifteen, display);
+};
+
+
+function scoreCount(){
+    var score = x;
+    if document.getElementById("correct").onclick{
+        return score++;
+    }
+    else timer = document.getElementById("timer").innerText = (oneFifteen - 15000ms)
+
+};
+
+
+
 //create variable for questions and correct answers
 //create array of questions to append to container sequentially
-//create variable for score and if input=(".correct"){score++} else{timer-("15000ms")}
