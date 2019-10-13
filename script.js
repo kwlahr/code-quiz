7a4e55f714bd4e64f00ee4f5070b5a8996b2dfb1
kwlahr <kwlahr@gmail.com>
2019-10-13 14:40:26 -0600

    start button now functioning

--- script.js
+++ script.js
@@ -1,58 +1,39 @@
 //create variable for score and timer and if input=(".correct"){score++} else{timer-("15000ms")}
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
+//USE SET INTERVAL TIMER
+window.onload = function() {
+  function startTimer(duration, display) {
+    var seconds = 75;
+    document.getElementById("startCard").style.display = "none";
+    displayQuestion();
+    setInterval(function() {
+      document.getElementById("timer").innerText = "Time Remaining: " + seconds;
+      seconds--;
     }, 1000);
+  }
+
+  var start = document.getElementById("start");
+  console.log(start);
+  start.addEventListener("click", startTimer);
+
+  var score = 0;
+
+  
+  var displayedQuestion = 1;
+  
+  function displayQuestion() {
+      document.getElementById("q" + displayedQuestion).style.display = "block";
+      displayQuestion++;
+    }
+};
+
+var answerBtn = document.getElementsByClassName("answer")
+function answerSelect(event){
+    console.log(event)
 }
-
-//can't get this to work
-var start = document.getElementById("start")
-document.getElementById(start).addEventListener(('click'),startTimer (duration, display));{
-    var oneFifteen = 60 * 1.25,
-        display = document.querySelector('#timer');
-    startTimer(oneFifteen, display);
-};
-
-startTimer();
-
-
-function scoreCount(){
-    var score = 0;
-    if (document.getElementById('correct').onclick){
-        console.log ;score++;
+    for(var i = 0; i < answerBtn.length; i++){
+        answerBtn[i].addEventListener("click", answerSelect)
     }
-    else timer = document.getElementById("timer").innerText = (oneFifteen - "15000ms")
 
-};
-
-scoreCount();
-
-//create variable for questions and correct answers in questions.js
-
-//show one card at a time (no idea how to do this)
-
-//if user selects an answer show the next card (my best guess)
-function next(){
-    var questions = ["#start", "#q1", "#q2", "#q3", "#q4", "#q5", "#end"]
-    for (var i=0; i<questions.length; i++)
-    if (document.getElementsByClassName("btn").onclick){
-        container.append.questions;
-    }
-};
-
-
-
-
-
-
+// document.getElementById("answer").addEventListener("click", function(event){
+//   console.log(event);
+// })
