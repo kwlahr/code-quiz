ed6a301c58933ed25a18458eb5d8f4ab5649f9aa
kwlahr <kwlahr@gmail.com>
2019-10-14 22:24:21 -0600

    updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //create variable for score and timer and if input=(".correct"){score++} else{timer-("15000ms")}
 //USE SET INTERVAL TIMER
+var displayedQuestion = 0;
+var qContainer = document.getElementById("q");
+var score = 0;
+var seconds = 75;
 window.onload = function() {
   function startTimer(duration, display) {
-    var seconds = 75;
     document.getElementById("startCard").style.display = "none";
     displayQuestion();
     setInterval(function() {
@@ -15,25 +18,50 @@ window.onload = function() {
   console.log(start);
   start.addEventListener("click", startTimer);
 
-  var score = 0;
 
-  
-  var displayedQuestion = 1;
-  
+  console.log(qContainer);
+
   function displayQuestion() {
-      document.getElementById("q" + displayedQuestion).style.display = "block";
-      displayQuestion++;
+    var allButtons = document.getElementById("cardBody");
+    qContainer.textContent = questions[displayedQuestion].title;
+    // displayedQuestion++;
+    qContainer.style.display = "block";
+
+    // var answerBtn = document.getElementsByClassName("answer")
+    var answerBtn = questions[displayedQuestion].choices;
+    // function answerSelect(event){
+    //   console.log(event)
+    // }
+    for (var i = 0; i < answerBtn.length; i++) {
+      // answerBtn[i].addEventListener("click", answerSelect)
+      var newBtn = document.createElement("button");
+      newBtn.textContent = answerBtn[i];
+      newBtn.className += "options";
+      allButtons.appendChild(newBtn);
+
+      newBtn.addEventListener("click", function(event){
+        console.log(this.textContent);
+        if( this.textContent == questions[displayedQuestion].answer){
+          console.log("Correct!")
+        }
+        else{
+          seconds -= 15;
+        }
+        var elements = document.getElementsByClassName("options");
+        while(elements.length > 0){
+          elements[0].parentNode.removeChild(elements[0]);
+        }
+        displayedQuestion++;
+        displayQuestion();
+
+      })
     }
+  }
+
+  console.log(questions);
 };
 
-var answerBtn = document.getElementsByClassName("answer")
-function answerSelect(event){
-    console.log(event)
-}
-    for(var i = 0; i < answerBtn.length; i++){
-        answerBtn[i].addEventListener("click", answerSelect)
-    }
-
 // document.getElementById("answer").addEventListener("click", function(event){
+
 //   console.log(event);
 // })
